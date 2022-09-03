b2b486a3050eec5e99fd7b5b67c0270e2ea83724
yutahack <yutahack@gmail.com>
2022-09-03 23:42:25 +0900

    API Key 접근제한 기능 추가

--- app/index.js
+++ app/index.js
@@ -5,10 +5,15 @@ const cors = require("cors");
 require("dotenv").config({ path: "./.env" });
 
 const port = process.env.PORT;
+const key = process.env.X_API_KEY;
 app.use(cors());
 tasker.startScheduler();
 
 app.post("/ping", (req, res) => {
+    if (key !== req.headers["x-api-key"]) {
+        res.send("Invalid API Key");
+        return;
+    }
     var ping = tasker.getPingToHostResult();
     res.send(ping);
 });
